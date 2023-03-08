d9ba1c42c2fe024c4bb4fd8c2d9d31b933887857
Sukuna18 <gossfall10@gmail.com>
2023-03-08 16:23:57 +0000

    modification tp4 (1)

--- TP4/app.js
+++ TP4/app.js
@@ -7,6 +7,8 @@ let copy = document.querySelector("#copy");
 let mdp = document.querySelector("#password");
 let length = document.querySelector("#length");
 let form = document.querySelector("form");
+let notify = document.querySelector(".notification");
+let boutton = document.createElement("button");
 
 let lettreMaj = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let lettreMin = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -32,11 +34,22 @@ function generatePassword() {
   }
 
   if (allowedCharacters.length === 0) {
-    alert('Veuillez cocher au moins une option.');
+    
+    boutton.innerHTML = 'Veuillez cocher au moins une option.';
+    let clone = boutton.cloneNode(true);
+    notify.appendChild(clone);
+    setTimeout(() => {
+      notify.removeChild(clone);
+    },2000)
     return;
   }
   if(length.value > 20 || length.value < 0){
-    alert('Veuillez saisir un nombre superieur a 0 et inférieur à 20.');
+    boutton.innerHTML = 'Veuillez saisir un nombre superieur a 0 et inférieur à 20.';
+    let clone = boutton.cloneNode(true);
+    notify.appendChild(clone);
+    setTimeout(() => {
+      notify.removeChild(clone);
+    },2000)
     return;
   }
   for (let i = 0; i < length.value; i++) {
@@ -49,4 +62,10 @@ function generatePassword() {
 // Événement pour générer un mot de passe lorsqu'on clique sur le bouton "Générer un Mot de Passe"
 generate.addEventListener('click', generatePassword);
 
-
+copy.addEventListener("click", copyInputValue)
+function copyInputValue(){
+ 
+    mdp.select();
+  document.execCommand("copy");
+  alert("la valeur a ete copier dans le presse papier");
+}
